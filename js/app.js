38dbc0ff9ec5af7f7112c59b87196a558ad957fd
olegonyxov <olegonyxov@gmail.com>
2024-04-01 01:07:31 +0300

    222

--- js/app.js
+++ js/app.js
@@ -26,36 +26,34 @@ function showUsersFunc (){
     let userRaw = document.createElement("raw")
     const storedUser = localStorage.getItem('users')
     const parsedUser = JSON.parse(storedUser)
-    for (user in parsedUser){
-        userRaw.textContent= parsedUser.firstName
-        users_Raw.appendChild(userRaw)
-        let view_btn = document.createElement("input")
-        view_btn.setAttribute('type',"button")
-        view_btn.setAttribute('value',"VIEW")
-        view_btn.setAttribute('class', 'view_btn')
-        userRaw.appendChild(view_btn)
-        let edit_btn = document.createElement("input")
-        edit_btn.setAttribute('type',"button")
-        edit_btn.setAttribute('value',"EDIT")
-        userRaw.appendChild(edit_btn)
-        let remove_btn = document.createElement("input")
-        remove_btn.setAttribute('type',"button")
-        remove_btn.setAttribute('value',"REMOVE")
-        userRaw.appendChild(remove_btn)
-    }  
+    // userRaw.textContent= parsedUser.firstName
+    users_Raw.appendChild(userRaw)
+    let view_btn = document.createElement("input")
+    view_btn.setAttribute('type',"button")
+    view_btn.setAttribute('value',"VIEW")
+    view_btn.setAttribute('class', 'view_btn')
+    userRaw.appendChild(view_btn)
+    let edit_btn = document.createElement("input")
+    edit_btn.setAttribute('type',"button")
+    edit_btn.setAttribute('value',"EDIT")
+    userRaw.appendChild(edit_btn)
+    let remove_btn = document.createElement("input")
+    remove_btn.setAttribute('type',"button")
+    remove_btn.setAttribute('value',"REMOVE")
+    userRaw.appendChild(remove_btn)
+     
 
 }
 
 
 function addNewUser(){
     user = collectData()
-    users = localStorage.getItem('users' || [])
-    parsedUsers = JSON.parse(users)
-    console.log(parsedUsers)
-    console.log(user)
-    parsedUsers.push(user)
+    if (localStorage.getItem('users')) {
+        parsedUsers = JSON.parse(localStorage.getItem('users'));
+        parsedUsers.push(user) //Почему ОНО не ПУШИТ аналогичное значение???
+        localStorage.setItem("users", JSON.stringify(parsedUsers));
+    } else { localStorage.setItem("users", JSON.stringify(user))}
     
-    localStorage.setItem("users", JSON.stringify(parsedUsers))
 
 }
 
